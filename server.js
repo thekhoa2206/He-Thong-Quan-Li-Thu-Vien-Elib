265b3665f752577c792df17e56ebdd07384704fd
unknown <thekhoa2206@gmail.com>
2020-05-10 11:03:35 +0700

    add middleware, /library, logout

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const port = 3000
-const app = express()
 var readerRoute = require('./routes/reader.route');
 var bookRoute = require('./routes/book.route');
 const cookieParser = require('cookie-parser');
+var authRoute = require('./routes/auth.route');
 
+const app = express()
+
+
+var authMiddleware = require('./middlewares/auth.middleware')
 
 app.set('views', './views')
 app.set('view engine', 'pug')
@@ -13,13 +17,23 @@ app.use(cookieParser('dakshdksajfhdskj')) // use to read format cookie
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.use('/readers',  readerRoute);
+app.use(express.static('public'))
 
+app.use('/readers',authMiddleware.adminAuth, readerRoute);
+app.use('/books', authMiddleware.adminAuth, bookRoute)
+app.use('/auth', authRoute)
 
 // index page 
 app.get('/', function(req, res) {
     res.render('index');
 });
+app.get('/library',authMiddleware.adminAuth, function(req, res) {
+    res.render('library');
+});
+app.get('/logout', function(req, res){
+	res.clearCookie("readerId");
+	res.redirect('/')
+})
 
 
 app.listen(port, () => console.log('Example app listening at http://localhost:' + port))
