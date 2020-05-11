31f7093a67577a776d368cbc24df7cc94217a317
unknown <thekhoa2206@gmail.com>
2020-05-11 21:23:40 +0700

    phan quyen admin, thu thu, reader

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,20 +1,6 @@
 var mysql = require('mysql')
 var con = require('../mysql-connection')
 
-module.exports.requireAuth = function (req, res, next){
-	if(!req.signedCookies.readerId){
-		res.redirect('/auth/login');
-		return;
-	}
-	con.query('SELECT * FROM readers WHERE readerId = ?', req.signedCookies.readerId, function (err, result) { 
-  		if (err) throw err;
-  		if(!result[0]){
-  			res.redirect('/auth/login');
-  		}
-  		res.locals.reader = result[0];
-  		next()
-	});
-};
 module.exports.adminAuth = function (req, res, next){
 	if(!req.signedCookies.readerId){
 		res.redirect('/auth/login');
@@ -22,8 +8,37 @@ module.exports.adminAuth = function (req, res, next){
 	}
 	con.query('SELECT * FROM readers WHERE readerId = ?', req.signedCookies.readerId, function (err, result) { 
   		if (err) throw err;
-		if (result[0].readerId.localeCompare("11111111")!== 0){
-			res.redirect('')
+		if (result[0].userId == 1){
+			res.redirect('/library')
+	}
+	res.locals.reader = result[0];
+	next()
+	});
+};
+module.exports.readerAuth = function (req, res, next){
+	if(!req.signedCookies.readerId){
+		res.redirect('/auth/login');
+		return;
+	}
+	con.query('SELECT * FROM readers WHERE readerId = ?', req.signedCookies.readerId, function (err, result) { 
+  		if (err) throw err;
+		if (result[0].userId == 3){
+			res.redirect('/library')
+	}
+	res.locals.reader = result[0];
+	next()
+	});
+};
+
+module.exports.librarianAuth = function (req, res, next){
+	if(!req.signedCookies.readerId){
+		res.redirect('/auth/login');
+		return;
+	}
+	con.query('SELECT * FROM readers WHERE readerId = ?', req.signedCookies.readerId, function (err, result) { 
+  		if (err) throw err;
+		if (result[0].userId == 2){
+			res.redirect('/borrowing')
 	}
 	res.locals.reader = result[0];
 	next()
