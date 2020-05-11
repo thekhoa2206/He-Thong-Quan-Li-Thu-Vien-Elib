bab05b89ae70f12ebf39c7a06e1503396b5994da
unknown <thekhoa2206@gmail.com>
2020-05-11 21:30:51 +0700

    borrow.route

---
+++ routes/borrow.route.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+var router = express.Router();
+var controller = require('../controllers/borrowBook.controller');
+
+router.get('/', controller.borrowBooks);
+
+module.exports = router;
