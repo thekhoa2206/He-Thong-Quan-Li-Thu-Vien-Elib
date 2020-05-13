beadb74067abc3833fe80cad961e8d71bee58990
unknown <thekhoa2206@gmail.com>
2020-05-13 20:20:11 +0700

    server.js

--- server.js
+++ server.js
@@ -1,41 +1,54 @@
 const express = require('express')
 const port = 3000
+var bodyParser = require('body-parser');
 var readerRoute = require('./routes/reader.route');
 var bookRoute = require('./routes/book.route');
 var borrowingRoute = require('./routes/borrowReader.route');
 var borrowBookRoute = require('./routes/borrow.route');
-const cookieParser = require('cookie-parser');
+//const cookieParser = require('cookie-parser');
 var authRoute = require('./routes/auth.route');
 var session = require('express-session')
 const app = express()
-
+var passport = require('passport');
 
 var authMiddleware = require('./middlewares/auth.middleware')
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.use(cookieParser('dakshdksajfhdskj')) // use to read format cookie
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
 
-app.use('/readers',authMiddleware.librarianAuth, readerRoute);
-app.use('/books', authMiddleware.librarianAuth, bookRoute);
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+app.use('/readers',readerRoute);
+app.use('/books', bookRoute);
 app.use('/borrowing', authMiddleware.readerAuth, borrowingRoute);
-app.use('/viewBorrowReader', authMiddleware.librarianAuth, borrowBookRoute);
+app.use('/viewBorrowReader',  borrowBookRoute);
 app.use('/auth', authRoute)
 
 // index page 
 app.get('/', function(req, res) {
     res.render('index');
 });
-app.get('/library',authMiddleware.librarianAuth, function(req, res) {
+app.get('/library',authMiddleware.adminAuth, function(req, res) {
     res.render('library');
 });
 app.get('/logout', function(req, res){
-	res.clearCookie("readerId");
+	res.clearCookie("connect.sid");
 	res.redirect('/')
 })
 
