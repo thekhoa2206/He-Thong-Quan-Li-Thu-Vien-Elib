a98ebd41e5e051a1c6738b8abc84338c097f3b1d
unknown <thekhoa2206@gmail.com>
2020-05-14 16:29:44 +0700

    server.js

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express')
 const port = 3000
-var bodyParser = require('body-parser');
 var readerRoute = require('./routes/reader.route');
 var bookRoute = require('./routes/book.route');
 var borrowingRoute = require('./routes/borrowReader.route');
 var borrowBookRoute = require('./routes/borrow.route');
+var librarianRoute = require('./routes/librarian.route')
 //const cookieParser = require('cookie-parser');
 var authRoute = require('./routes/auth.route');
+
+
 var session = require('express-session')
 const app = express()
 var passport = require('passport');
 
 var authMiddleware = require('./middlewares/auth.middleware')
+var authenticateMiddleware = require('./middlewares/authenticate.middleware')
+
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(passport.initialize());
-// app.use(passport.session());
+passport.serializeUser((user, done) => {
+    done(null, user)
+})
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -34,23 +42,24 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use('/readers',readerRoute);
-app.use('/books', bookRoute);
-app.use('/borrowing', authMiddleware.readerAuth, borrowingRoute);
-app.use('/viewBorrowReader',  borrowBookRoute);
+app.use('/readers',authMiddleware.readerAuth, authenticateMiddleware.librarianAuth, readerRoute);
+app.use('/books', authMiddleware.readerAuth, authenticateMiddleware.librarianAuth, bookRoute);
+app.use('/borrowing',authMiddleware.readerAuth,authenticateMiddleware.readerAuth, borrowingRoute);
+app.use('/viewBorrowReader',authMiddleware.readerAuth ,authenticateMiddleware.librarianAuth,  borrowBookRoute);
+app.use('/librarians',authMiddleware.readerAuth ,authenticateMiddleware.adminAuth, librarianRoute)
 app.use('/auth', authRoute)
-
 // index page 
 app.get('/', function(req, res) {
     res.render('index');
 });
-app.get('/library',authMiddleware.adminAuth, function(req, res) {
+app.get('/library',authMiddleware.readerAuth ,authenticateMiddleware.librarianAuth, function(req, res) {
     res.render('library');
 });
+
 app.get('/logout', function(req, res){
 	res.clearCookie("connect.sid");
 	res.redirect('/')
-})
+});
 
 
 app.listen(port, () => console.log('Example app listening at http://localhost:' + port))
