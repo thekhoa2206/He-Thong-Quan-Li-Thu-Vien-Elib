95f8c80486af5f877a7bb55f9aada43d03a442ed
unknown <thekhoa2206@gmail.com>
2020-05-30 09:54:59 +0700

    controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,22 +11,7 @@ module.exports.postLogin = function (req, res, next) {
 	var password = req.body.password;
 	con.query('SELECT * FROM readers WHERE readerId = ?',[readerId], function (err, result) { //result trả về 1 array chứa object
 		//console.log(typeof result[0].username)
-
-     var ngayHetHan = result[0].ngayHetHan;
-
-      x =  ngayHetHan.slice(0, 2);
-      y =  ngayHetHan.slice(3, 5);
-      z =  ngayHetHan.slice(6,10);
-
-      var g = new Date();
-      g.setDate(x);
-      g.setMonth(y-1);
-      g.setFullYear(z);
-       var date = new Date(g);
-      var e = date.getTime();
-      var now = new Date();
-      var f = now.getTime();
-      if(f <= e){
+  
 		if (err) throw err;
 		if(result[0] === undefined || result[0].readerId !== readerId) {
 			res.render('auth/login', {
@@ -49,6 +34,23 @@ module.exports.postLogin = function (req, res, next) {
 			});
 			return;
 		}
+			  var ngayHetHan = result[0].ngayHetHan;
+
+      x =  ngayHetHan.slice(0, 2);
+      y =  ngayHetHan.slice(3, 5);
+      z =  ngayHetHan.slice(6,10);
+
+      var g = new Date();
+      g.setDate(x);
+      g.setMonth(y-1);
+      g.setFullYear(z);
+       var date = new Date(g);
+      var e = date.getTime();
+      var now = new Date();
+      var f = now.getTime();
+		 if(f <= e){
+
+
 		req.session.readerId = result[0].readerId;
 
 		//req.session.username = result[0].username;
--- controllers/authG.controller.js
+++ controllers/authG.controller.js
@@ -12,21 +12,6 @@ module.exports.postLogin = function (req, res, next) {
 	con.query('SELECT * FROM readers WHERE readerId = ?',[readerId], function (err, result) { //result trả về 1 array chứa object
 		//console.log(typeof result[0].username)
 
-     var ngayHetHan = result[0].ngayHetHan;
-
-      x =  ngayHetHan.slice(0, 2);
-      y =  ngayHetHan.slice(3, 5);
-      z =  ngayHetHan.slice(6,10);
-
-      var g = new Date();
-      g.setDate(x);
-      g.setMonth(y-1);
-      g.setFullYear(z);
-       var date = new Date(g);
-      var e = date.getTime();
-      var now = new Date();
-      var f = now.getTime();
-      if(f <= e){
 		if (err) throw err;
 		if(result[0] === undefined || result[0].readerId !== readerId) {
 			res.render('auth/login', {
@@ -58,7 +43,21 @@ module.exports.postLogin = function (req, res, next) {
 			return;
 		}
 		req.session.readerId = result[0].readerId;
+     var ngayHetHan = result[0].ngayHetHan;
 
+      x =  ngayHetHan.slice(0, 2);
+      y =  ngayHetHan.slice(3, 5);
+      z =  ngayHetHan.slice(6,10);
+
+      var g = new Date();
+      g.setDate(x);
+      g.setMonth(y-1);
+      g.setFullYear(z);
+       var date = new Date(g);
+      var e = date.getTime();
+      var now = new Date();
+      var f = now.getTime();
+      if(f <= e){
 		//req.session.username = result[0].username;
 		console.log(result[0].userId)
 	} else {
